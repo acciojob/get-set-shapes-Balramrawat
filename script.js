9d9ae0cf756b9d7677a2967e70cb739a5a109db4
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-03-05 12:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.heigth=height;
+		if(width<=0 || heigth<=0){
+			throw new error("heigth and width are positive...")
+		}
 
-class Square extends Animal {}
+		get width(){
+			return this.width;
+		}
+		get heigth(){
+			return this.heigth;
+		}
+		getarea(){
+			return this.width * this.heigth
+		}
+	}
+}
+
+class Square extends Animal {
+	constructor(side){
+		if(side<=0){
+			throw new error("side are positive...")
+		}
+
+		super(side,side)
+
+		getPerimeter(){
+			return 4*this.width
+		}
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
+
